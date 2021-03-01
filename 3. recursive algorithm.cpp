bcf5643ab32172614fa1258f843d7353509c7248
Mira Shanouda <Mira.shanouda@aucegypt.edu>
2021-03-01 17:12:21 +0200

    adding bubble, selection and insertion counters

--- 3. recursive algorithm.cpp
+++ 3. recursive algorithm.cpp
@@ -2,11 +2,20 @@
 #include <algorithm>
 using namespace std;
 
+//All functions:
+int heapify(int arr[], int n, int i, int count = 0);
+int build_heap(int arr[], int n);
+int heapSort(int arr[], int n);
+int heap_sort_count(int[], int);
+int bubble_sort_count(int* arr, int n);
+int insertion_sort_count(int* arr, int n);
+int selectionSort_count(int arr[], int n);
+
+
 //to rearrange a sub tree with a root i where i is the index of the tree
 // n is the size
 //we use this function to creat a max-heap in a bottom-up manner
-int heapify(int arr[], int n, int i, int count = 0);
-int heapify(int arr[], int n, int i, int count = 0) {
+int heapify(int arr[], int n, int i, int count) {
 	
 	int largest = i; //initialize the ragest as the root
 	int left = 2 * i + 1;
@@ -29,19 +38,18 @@ int heapify(int arr[], int n, int i, int count = 0) {
 }
 
 
-int build_heap(int arr[], int n);
 int build_heap(int arr[], int n) {
 
 	int count = 0;
 	//start calling from the last node that has children
 	//leaves start from index n/2 which means if we have 10 elements then the leaves starts from 6 to 10 (n/2+1) 
 	for (int i = n / 2 - 1; i >= 0; i--) {
-		int count = count + heapify(arr, n, i);
+		count = count + heapify(arr, n, i);
 	}
 	return count;
 }
 
-int heapSort(int arr[], int n);
+
 int heapSort(int arr[], int n) {
 	
 	int count = build_heap(arr, n);
@@ -54,8 +62,63 @@ int heapSort(int arr[], int n) {
 }
 
 
-int heap_sort_count(int[], int);
 int heap_sort_count(int arr[], int n)
 {	
 	return heapSort(arr, n);
+}
+
+int bubble_sort_count(int* arr, int n) {
+
+	int count_bubble = 0;
+	for (int i = 0; i < n - 1; i++) {//the last element is already in place
+		for (int j = 0; j < n - i - 1; j++) {
+			count_bubble++; //counts the number of comparisions 
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+	return count_bubble;
+}
+
+int insertion_sort_count(int* arr, int n) {
+
+	int count_insert = 0, i, j, key; //key refers to the smallest element
+
+	for (i = 1; i < n; i++) {//starts from 1 becasue we are comparing with the previous one
+
+		key = arr[i];
+		j = i - 1;
+
+		count_insert++;
+		while (j >= 0) {
+			if (arr[j] > key) {
+
+				arr[j + 1] = arr[j];
+			}
+			j--;
+		}
+		arr[j] = key;//*******************************
+	}
+	return count_insert;
+}
+
+int selectionSort_count(int arr[], int n)
+{
+	int i, j, min_idx, count = 0;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+		{
+			count++;
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+			swap(arr[min_idx], arr[i]);
+	}
+	return count;
 }
